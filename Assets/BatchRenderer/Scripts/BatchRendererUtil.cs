8864669267a9bb3a8946122d2a17dda53551deab
i-saint <saint.skr@gmail.com>
2015-01-23 22:36:00 +0900

    added ComputeUVOsset variations

--- Assets/BatchRenderer/Scripts/BatchRendererUtil.cs
+++ Assets/BatchRenderer/Scripts/BatchRendererUtil.cs
@@ -8,8 +8,19 @@ public static class BatchRendererUtil
 {
     public static Vector4 ComputeUVOsset(Texture texture, Rect rect)
     {
-        float tw = (float)texture.width;
-        float th = (float)texture.height;
+        float tw = texture.width;
+        float th = texture.height;
+        return new Vector4(
+            rect.width / tw,
+            rect.height / th,
+            rect.xMin / tw,
+            (1.0f - rect.yMax) / th);
+    }
+
+    public static Vector4 ComputeUVOsset(int texture_width, int texture_height, Rect rect)
+    {
+        float tw = texture_width;
+        float th = texture_height;
         return new Vector4(
             rect.width / tw,
             rect.height / th,
